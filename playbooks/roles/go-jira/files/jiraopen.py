0481bd4dd8484219368f54ed66cc37bd343967bc
0x29a <grrrr@protonmail.com>
2022-07-24 02:30:27 +0300

    feat: improve jiraopen

--- playbooks/roles/go-jira/files/jiraopen.py
+++ playbooks/roles/go-jira/files/jiraopen.py
@@ -5,12 +5,19 @@ import webbrowser
 
 def main():
     clipboard = os.popen("xclip -selection clipboard -o")
-    ticket = re.search(r"[A-Z]{2,}-\d+", clipboard.read())
-    webbrowser.open(
-        "https://tasks.opencraft.com/browse/" + ticket.group(0)
-    ) if ticket else None
+    clipboard_content = clipboard.read()
     clipboard.close()
 
+    ticket = re.search(r"[A-Z]{2,}-\d+", clipboard_content)
+    if ticket:
+        webbrowser.open(
+            "https://tasks.opencraft.com/browse/" + ticket.group(0)
+        )
+        return
+
+    url = re.search("(?P<url>https?://[^\s]+)", clipboard_content)
+    if url:
+        webbrowser.open(url.group("url"))
 
 if __name__ == "__main__":
     main()
